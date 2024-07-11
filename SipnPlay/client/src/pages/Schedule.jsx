cd1b2af182b26b04d155cad5958366d49283896f
Shema Dabiri <dabirishema@gmail.com>
2024-07-11 14:03:03 -0600

    vercel librariy imported

---
+++ SipnPlay/client/src/pages/Schedule.jsx
@@ -0,0 +1,17 @@
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+
+
+function Schedule() {
+  return (
+    <>
+    Schedule
+    
+    </>
+  )
+}
+
+export default Schedule
