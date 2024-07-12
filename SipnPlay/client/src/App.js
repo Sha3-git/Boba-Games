b6bd8ed9c82ed2dc539567dcb7f7f7b8dc9078aa
Shema Dabiri <dabirishema@gmail.com>
2024-07-11 18:40:07 -0600

    header and home setup

--- SipnPlay/client/src/App.js
+++ SipnPlay/client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Menu from './pages/Menu';
 import Schedule from './pages/Schedule';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/"element={<Home/>} />
           <Route path="/schedule" element={<Schedule/>} />
           <Route path="/Menu"element={<Menu/>} />
         </Routes>
