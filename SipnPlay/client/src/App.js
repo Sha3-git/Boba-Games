c3e54f334ec694c24dfa66099f73d056e322e2dc
Laura <ladeezn11@gmail.com>
2024-07-12 15:45:28 +0100

    game-laura

--- SipnPlay/client/src/App.js
+++ SipnPlay/client/src/App.js
@@ -1,26 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Menu from './pages/Menu';
-import Schedule from './pages/Schedule';
+import logo from "./logo.svg"
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Menu from "./pages/Menu"
+import Schedule from "./pages/Schedule"
+import Games from "./pages/Games"
 
-import CalEvent from './components/CalEvent';
+import CalEvent from "./components/CalEvent"
 
-import Home from './pages/Home';
+import Home from "./pages/Home"
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/"element={<Home/>} />
-          <Route path="/schedule" element={<Schedule/>} />
-          <Route path="/Menu"element={<Menu/>} />
-          <Route path="/Calendar"element={<Schedule />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/Calendar" element={<Schedule />} />
+          <Route path="/games" element={<Games />} />
         </Routes>
-
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ SipnPlay/client/src/pages/Games.jsx
@@ -0,0 +1,129 @@
+import "./About.css"
+import Header from "../components/Header"
+import someonecool from "../assets/images/someone.cool.png"
+import bobahappy from "../assets/images/boba-happy.png"
+
+function Games() {
+  return (
+    <>
+      <Header />
+      <section class="boardgame">
+        <div class="card">
+          <div class="imgBg">
+            <img src="img/catan@2x.png" alt="" />
+          </div>
+          <div class="content">
+            <div class="details">
+              <h2>
+                CATAN <br />
+                <span>Something about something</span>
+              </h2>
+              <div class="data">
+                <h3>
+                  Players
+                  <br />
+                  <span>3-4</span>
+                </h3>
+                <h3>
+                  Age
+                  <br />
+                  <span>10+</span>
+                </h3>
+                <h3>
+                  Play Time
+                  <br />
+                  <span>2 hrs</span>
+                </h3>
+              </div>
+              <div class="description">
+                <h3>
+                  Description
+                  <br />
+                  <span>Settlers of Catan is a strategy board game involving smart resource management.</span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="imgBg">
+            <img src="img/catan@2x.png" alt="" />
+          </div>
+          <div class="content">
+            <div class="details">
+              <h2>
+                CATAN <br />
+                <span>Something about something</span>
+              </h2>
+              <div class="data">
+                <h3>
+                  Players
+                  <br />
+                  <span>3-4</span>
+                </h3>
+                <h3>
+                  Age
+                  <br />
+                  <span>10+</span>
+                </h3>
+                <h3>
+                  Play Time
+                  <br />
+                  <span>2 hrs</span>
+                </h3>
+              </div>
+              <div class="description">
+                <h3>
+                  Description
+                  <br />
+                  <span>Settlers of Catan is a strategy board game involving smart resource management.</span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card">
+          <div class="imgBg">
+            <img src="img/catan@2x.png" alt="" />
+          </div>
+          <div class="content">
+            <div class="details">
+              <h2>
+                CATAN <br />
+                <span>Something about something</span>
+              </h2>
+              <div class="data">
+                <h3>
+                  Players
+                  <br />
+                  <span>3-4</span>
+                </h3>
+                <h3>
+                  Age
+                  <br />
+                  <span>10+</span>
+                </h3>
+                <h3>
+                  Play Time
+                  <br />
+                  <span>2 hrs</span>
+                </h3>
+              </div>
+              <div class="description">
+                <h3>
+                  Description
+                  <br />
+                  <span>Settlers of Catan is a strategy board game involving smart resource management.</span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Games
