43e3d33ee5b3386e17fea601cb087b660df3dce1
Laura <ladeezn11@gmail.com>
2024-07-12 15:15:16 +0100

    about page

--- SipnPlay/client/src/App.js
+++ SipnPlay/client/src/App.js
@@ -1,26 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Menu from './pages/Menu';
-import Schedule from './pages/Schedule';
+import logo from "./logo.svg"
+import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Menu from "./pages/Menu"
+import Schedule from "./pages/Schedule"
+import About from "./pages/About"
 
-import CalEvent from './components/CalEvent';
+import CalEvent from "./components/CalEvent"
 
-import Home from './pages/Home';
+import Home from "./pages/Home"
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/"element={<Home/>} />
-          <Route path="/schedule" element={<Schedule/>} />
-          <Route path="/Menu"element={<Menu/>} />
-          <Route path="/Calendar"element={<Schedule />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/schedule" element={<Schedule />} />
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/Calendar" element={<Schedule />} />
+          <Route path="/about" element={<About />} />
         </Routes>
-
       </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
